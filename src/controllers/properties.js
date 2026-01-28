5f74c5d84f0503953873d390b5eda2ae2f41cb87
Amrit Bhardwaj <amritbharadwaj4@gmail.com>
2026-01-28 09:37:09 +0530

    all apis

--- src/controllers/properties.js
+++ src/controllers/properties.js
@@ -64,6 +64,78 @@ const getdatabyname = async (req, res) => {
       message: error.message
     });
   }
+};  
+const updatedetails = async (req, res) => {
+  try {
+    const { house_id, ...updateData } = req.body;
+
+    if (!house_id) {
+      return res.status(400).json({
+        message: "house_id is required"
+      });
+    }
+
+    const updatedHouse = await House.findOneAndUpdate(
+      { house_id: house_id },
+      { $set: updateData },
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!updatedHouse) {
+      return res.status(404).json({
+        message: "property not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "property updated successfully",
+      data: updatedHouse
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+};
+const deletebyid = async (req, res) => {
+  try {
+    const { house_id } = req.params;
+
+    if (!house_id) {
+      return res.status(400).json({
+        success: false,
+        message: "house_id is required"
+      });
+    }
+
+    const deletedHouse = await House.findOneAndDelete({
+      house_id: house_id
+    });
+
+    if (!deletedHouse) {
+      return res.status(404).json({
+        success: false,
+        message: "property not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "property deleted successfully",
+      data: deletedHouse
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
 };
 
-module.exports={properties,getdatabyid,getdatabyname};
+// some fields that cant be changed or updated are price_inr house id 
+module.exports={properties,getdatabyid,getdatabyname,updatedetails,deletebyid};
--- src/routes/properties.js
+++ src/routes/properties.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
-const {properties,getdatabyid,getdatabyname}=require('../controllers/properties')
+const {properties,getdatabyid,getdatabyname,updatedetails,deletebyid}=require('../controllers/properties')
 router.route("/").post(properties);
-
 router.route("/getbyname").get(getdatabyname);
+router.route("/update").patch(updatedetails);
+router.route("/delete/:house_id").delete(deletebyid);
 //always put this route down 
 router.route("/:id").get(getdatabyid);
 module.exports=router;
