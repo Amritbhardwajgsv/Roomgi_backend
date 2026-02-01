33c1f0c04d6f5d2b9808a779d6d2c2975a05cc30
Amrit Bhardwaj <amritbharadwaj4@gmail.com>
2026-02-01 09:34:10 +0530

    Change getbyid route from POST to GET

--- src/routes/properties.js
+++ src/routes/properties.js
@@ -14,7 +14,7 @@ const {
 
 router.post("/", auth, properties);
 
-router.post("/getbyid", auth, getdatabyid);
+router.get("/getbyid", auth, getdatabyid);
 
 router.post("/getbyname", auth, getdatabyname);
 
